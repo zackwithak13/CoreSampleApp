51b3889a5bbc82a4a44adaf3c2c11b9862d687e5
Zackariah Ruppert <zackariah.ruppert@lmcu.org>
2018-06-25 09:03:49 -0400

    Add project files.

---
+++ CoreSampleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CoreSampleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
