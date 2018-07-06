3eb6be02f1a26ea014fb8e0f7c213c7533d4addd
Zack Ruppert <zackruppert@gmail.com>
2018-07-06 08:59:12 -0400

    and intranet mvc core project

---
+++ CoreSampleApp.IntranetWeb/CodeFolder/SimpleInjector/CoreSampleAppIntranetWebInjectorModule.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Configuration;
+using CoreSampleApp.Business.SimpleInjector;
+using CoreSampleApp.Utilities.SimpleInjector;
+using SimpleInjector;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace CoreSampleApp.SimpleInjector
+{
+    public static class CoreSampleAppIntranetWebInjectorModule
+    {
+        public static void LoadTypes(Container container)
+        {
+            SimpleInjectorAccessor.Load(CoreSampleAppBusinessInjectorModule.LoadTypes);
+        }
+    }
+}
---
+++ CoreSampleApp.IntranetWeb/Startup.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CoreSampleApp.SimpleInjector;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Controllers;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SimpleInjector;
+using SimpleInjector.Integration.AspNetCore.Mvc;
+using SimpleInjector.Lifestyles;
+
+namespace CoreSampleApp.IntranetWeb
+{
+    public class Startup
+    {
+        private Container container = new Container();
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+
+            IntegrateSimpleInjector(services);
+        }
+
+
+        // see https://simpleinjector.readthedocs.io/en/latest/aspnetintegration.html
+        // for SimpleInjector Integration
+        private void IntegrateSimpleInjector(IServiceCollection services)
+        {
+            container.Options.DefaultScopedLifestyle = new AsyncScopedLifestyle();
+
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+
+            services.AddSingleton<IControllerActivator>(
+                new SimpleInjectorControllerActivator(container));
+            services.AddSingleton<IViewComponentActivator>(
+                new SimpleInjectorViewComponentActivator(container));
+
+            services.EnableSimpleInjectorCrossWiring(container);
+            services.UseSimpleInjectorAspNetRequestScoping(container);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            InitializeContainer(app);
+
+            //// Add custom middleware
+            //app.UseMiddleware<CustomMiddleware1>(container);
+            //app.UseMiddleware<CustomMiddleware2>(container);
+
+            container.Verify();
+
+            // ASP.NET default stuff here
+            if (env.IsDevelopment())
+            {
+                app.UseBrowserLink();
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+
+        private void InitializeContainer(IApplicationBuilder app)
+        {
+            // Add application presentation components:
+            container.RegisterMvcControllers(app);
+            container.RegisterMvcViewComponents(app);
+
+            container.Register<IConfiguration>(() => Configuration);
+            // Add application services.
+            CoreSampleAppIntranetWebInjectorModule.LoadTypes(container);
+
+            // Allow Simple Injector to resolve services from ASP.NET Core.
+            container.AutoCrossWireAspNetComponents(app);
+        }
+    }
+}
